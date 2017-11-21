a8391f0bca13579785ed78a3a8d40412ef62fde5
Steven Vandegrift <svande274@kcai.edu>
2017-11-20 22:33:11 -0600

    Added a bunch of stuff

---
+++ public/steven/codes.js
@@ -0,0 +1,36 @@
+var slider = document.getElementById("myRange");
+var value;
+var value2;
+var socket = io();
+socket.on('news', function (data) {
+  console.log(data);
+
+});
+
+function turnOn()
+{
+  console.log("Turning on...");
+  socket.emit('msg', "10");
+}
+
+function turnOff()
+{
+  console.log("Turning off...");
+  socket.emit('msg', "0");
+}
+
+value = slider.value;
+value2 = slider2.value;
+slider.oninput = function()
+{
+  value = this.value;
+  console.log(value);
+}
+slider2.oninput = function()
+{
+  value = this.value;
+  console.log(value);
+}
+function buttonPressed(){
+
+}
